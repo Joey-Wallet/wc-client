7219e51550ecc5f2420f828fc83b24e1258a21ad
Hussenet Thomas <113238884+LeJamon@users.noreply.github.com>
2025-09-08 15:12:40 +0200

    Fix: broken package json

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,15 +1,48 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import { createBaseConfig } from '../../config/vite.config.base.js'
 
-export default defineConfig(
-  createBaseConfig({
+export default defineConfig({
+  ...createBaseConfig({
     packageName: '@joey-wallet/wc-client',
     external: [
       '@walletconnect/types',
       '@walletconnect/universal-provider',
       '@walletconnect/utils',
       'eventemitter3',
+      '@joey-wallet/wc-core',
       '@joey-wallet/wc-core/utils',
+      '@joey-wallet/wc-react',
+      '@joey-wallet/wc-react/standalone',
     ],
-  })
-)
+  }),
+  build: {
+    ...createBaseConfig({
+      packageName: '@joey-wallet/wc-client',
+    }).build,
+    lib: {
+      entry: {
+        index: resolve(process.cwd(), 'src/index.ts'),
+        core: resolve(process.cwd(), 'src/core.ts'),
+        utils: resolve(process.cwd(), 'src/utils.ts'),
+        react: resolve(process.cwd(), 'src/react.ts'),
+      },
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      ...createBaseConfig({
+        packageName: '@joey-wallet/wc-client',
+      }).build?.rollupOptions,
+      external: [
+        '@walletconnect/types',
+        '@walletconnect/universal-provider',
+        '@walletconnect/utils',
+        'eventemitter3',
+        '@joey-wallet/wc-core',
+        '@joey-wallet/wc-core/utils',
+        '@joey-wallet/wc-react',
+        '@joey-wallet/wc-react/standalone',
+      ]
+    }
+  }
+})
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import { createReactConfig } from '../../config/vite.config.react.js'
 
-export default defineConfig(
-  createReactConfig({
+export default defineConfig({
+  ...createReactConfig({
     packageName: '@joey-wallet/wc-react',
     external: [
       'react',
@@ -12,6 +13,20 @@ export default defineConfig(
       '@walletconnect/universal-provider',
       '@walletconnect/utils',
       'eventemitter3',
+      '@joey-wallet/wc-core',
+      '@joey-wallet/wc-utils',
     ],
-  })
-)
+  }),
+  build: {
+    ...createReactConfig({
+      packageName: '@joey-wallet/wc-react',
+    }).build,
+    lib: {
+      entry: {
+        index: resolve(process.cwd(), 'src/index.ts'),
+        standalone: resolve(process.cwd(), 'src/standalone.ts'),
+      },
+      formats: ['es', 'cjs']
+    }
+  }
+})
